a6fbdb5cc8258fc1b2da10dff4565d2d92cd4e02
Omu Gwang num <omu@plantslaboratory.com>
2019-07-14 23:58:16 +0900

    URLからActionクラスとテンプレートを動的設定

--- src/Hanabi/Core/Controller.php
+++ src/Hanabi/Core/Controller.php
@@ -2,19 +2,90 @@
 
 namespace Hanabi\Core;
 
+use ReflectionClass;
 use Hanabi\Core\AppSmarty;
 
 class Controller
 {
-    public function __construct()
+    private $file;
+    private $ext = 'html';
+    private $dir;
+    private $action;
+    private $func;
+    
+    public function __construct($action)
     {
+        $this->action = $action;
     }
 
-    public static function fire()
+    // URL解析
+    private function parseUrl()
     {
+        if(isset($_SERVER['PATH_INFO'])) {
+            $uri = $_SERVER['PATH_INFO'];
+            if(preg_match('/(.*)\/$/', $uri)) {
+                $dir = $uri;
+                $file = 'index.html';
+            } else {
+                $dir = dirname($uri);
+                $file = basename($uri);
+            }
+        } else {
+            $dir = '/';
+            $file = 'index.html';
+        }
+        $dirs = explode('/', $dir);
+        $this->dir = [];
+        foreach($dirs as $dir) {
+            if($dir !== "")
+                $this->dir[] = $dir;
+        }
+
+        $arr = explode('.', $file);
+        $this->ext = $arr[1];
+        $arr = explode('_', $arr[0]);
+
+        $this->file = $arr[0];
+        if(isset($arr[1]))
+            $this->func = $arr[1];
+        else
+            $this->func = 'index';
+    }
+
+    // Actionクラス実行
+    private function internalAction()
+    {
+        $dir = "";
+        foreach($this->dir as $d) {
+            $dir .= '\\' . ucfirst($d);
+        }
+        $className = $this->action . $dir;
+        if(!class_exists($className))
+            return;
+
+        $class = new $className;
+        if(method_exists($class, $this->func)) {
+            $ref = new ReflectionClass($class);
+            $method = $ref->getMethod($this->func);
+            $params = $method->getParameters();
+            $funcParam = [];
+
+            $ret = $method->invokeArgs($class, $funcParam);
+        }
+    }
+
+    public static function fire($action)
+    {
+        $self = new static($action);
+        $self->parseUrl();
+        $self->internalAction();
+        
         $smarty = new AppSmarty();
-        $smarty->template_dir = 'public';
+        $smarty->template_dir = 'public' . '/' . implode('/', $self->dir);
         $smarty->compile_dir = 'cache';
-        $smarty->display('index.html');
+
+        // TODO: assign処理
+        
+        $smarty->display($self->file . '.' . $self->ext);
     }
 }
